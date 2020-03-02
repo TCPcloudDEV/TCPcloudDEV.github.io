e3e60e98ae2f5f3b13a9af3e130b4b98a6be3a26
TCPcloudDEV <29411098+TCPcloudDEV@users.noreply.github.com>
2020-03-01 18:39:11 -0600

    server

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const redirectToHTTPS = require('express-http-to-https').redirectToHTTPS;
 const FORECAST_DELAY = 0;
 
 // CODELAB: If running locally, set your Dark Sky API key here
-const API_KEY = process.env.DARKSKY_API_KEY;
+const API_KEY = ddf1b811918d15a6742ed89efc6625fb;
 const BASE_URL = `https://api.darksky.net/forecast`;
 
 // Fake forecast data used if we can't reach the Dark Sky API
