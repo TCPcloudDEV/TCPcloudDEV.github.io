369badd767ba439afb70b74d644412b662136fad
TCPcloudDEV <29411098+TCPcloudDEV@users.noreply.github.com>
2020-06-21 00:56:18 -0500

    init

--- public/init.js
+++ public/init.js
@@ -39,7 +39,7 @@
   */
   function initClient() {
     const CLIENT_ID =
-      "840179112792-bhg3k1h0dcnp9ltelj21o6vibphjcufe.apps.googleusercontent.com";
+      "405463250476-60n33ul1rrajp11mrimnp68og4qomueb.apps.googleusercontent.com";
     const DISCOVERY_DOCS = [
       "https://sheets.googleapis.com/$discovery/rest?version=v4",
       "https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"
