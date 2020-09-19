24f3fa0d5f1a4a04389fccb8d4ec97fa38caa4eb
TCPcloudDEV <29411098+TCPcloudDEV@users.noreply.github.com>
2020-09-19 18:15:21 -0500

    WIP

--- public/init.js
+++ public/init.js
@@ -45,7 +45,7 @@
   function initClient() {
     const CLIENT_ID =
       "405463250476-g6vgneqp01ht2pmv0mkqjr7qfds6k30l.apps.googleusercontent.com";
-    //const API_KEY = "AIzaSyB9lsuRV2xUnKU4mXwho34IZlZVxwu5qwQ";
+    const API_KEY = "AIzaSyB9lsuRV2xUnKU4mXwho34IZlZVxwu5qwQ";
     const DISCOVERY_DOCS = [
       "https://sheets.googleapis.com/$discovery/rest?version=v4",
       "https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"
@@ -55,7 +55,7 @@
     const SCOPES =
       "https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/drive.metadata.readonly";
 
-    //gapi.client.setApiKey(API_KEY);
+    gapi.client.setApiKey(API_KEY);
         
     gapi.client
       .init({
