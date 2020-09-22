b92f0d20f083ad7362ac5ec39110726819458cc5
TCPcloudDEV <29411098+TCPcloudDEV@users.noreply.github.com>
2020-09-21 19:57:54 -0500

    WIP

--- public/add-property.js
+++ public/add-property.js
@@ -1,97 +1,25 @@
-(function() {
-    const utils = window.ECSSales.utils;
+const utils = window.ECSSales.utils;
 
-    // Cached DOM bindings
-    const getElmById = document.getElementById.bind(document);
-    const AddPropertyForm = getElmById("add-property-form");
-    const oCustLastName = getElmById("cust-last-name");
-    const oCustFirstName = getElmById("cust-first-name");
-    const oCustAddr = getElmById("cust-addr");
-    const oCustCity = getElmById("cust-city");
-    const oCustPhone = getElmById("cust-phone");
-    const oCustEmail = getElmById("cust-email");
-    const oCustClaimNum = getElmById("job-claim-num");
-    const oJobSplitType = getElmById("job-split-type");
-    const oJobScope = getElmById("job-scope");
-    const oJobNotes = getElmById("job-notes");
-    const oClaimStatus = getElmById("job-claim-status");
+// Cached DOM bindings
+const getElmById = document.getElementById.bind(document);
+const AddPropertyForm = getElmById("add-property-form");
+const oCustLastName = getElmById("cust-last-name");
+const oCustFirstName = getElmById("cust-first-name");
+const oCustAddr = getElmById("cust-addr");
+const oCustCity = getElmById("cust-city");
+const oCustPhone = getElmById("cust-phone");
+const oCustEmail = getElmById("cust-email");
+const oCustClaimNum = getElmById("job-claim-num");
+const oJobSplitType = getElmById("job-split-type");
+const oJobScope = getElmById("job-scope");
+const oJobNotes = getElmById("job-notes");
+const oClaimStatus = getElmById("job-claim-status");
 
-    const oAddJobBttn = getElmById("add-job-bttn");
-    const oSnackbar = getElmById("toast-container");
+const oAddJobBttn = getElmById("add-job-bttn");
+const oSnackbar = getElmById("toast-container");
 
 
-    function addJob(event) {
-        if (!AddPropertyForm.checkValidity()) return false;
-
-        alert("addJob 2: " + event); // !!!!!!!!!!!!!!
-
-        event.preventDefault();
-        utils.showLoader();
-
-        var now = new Date();
-
-        gapi.client.sheets.spreadsheets.values
-            .append(
-                utils.appendRequestObj([
-                    [
-                        `=DATE(${now.getFullYear()}, ${now.getMonth()}, ${now.getDate()}) + TIME(${now.getHours()}, ${now.getMinutes()}, ${now.getSeconds()})`,
-                        oCustLastName.value,
-                        oCustFirstName.value,
-                        oCustAddr.value,
-                        oCustCity.value,
-                        oCustPhone.value,
-                        oCustEmail.value,
-                        oCustClaimNum.value,
-                        oJobSplitType.value,
-                        oJobScope.value,
-                        oJobNotes.value,
-                        oClaimStatus.value
-                    ]
-                ])
-            )
-            .then(
-                response => {
-                    // reset fileds
-                    oCustLastName.value = "";
-                    oCustFirstName.value = "";
-                    oCustAddr.value = "";
-                    oCustCity.value = "";
-                    oCustPhone.value = "";
-                    oCustEmail.value = "";
-                    oCustClaimNum.value = "";
-                    oJobSplitType.value = "";
-                    oJobScope.value = "";
-                    oJobNotes.value = "";
-                    oClaimStatus.value = "";
-
-                    oSnackbar.MaterialSnackbar.showSnackbar({
-                        message: "Property added!"
-                    });
-                    utils.hideLoader();
-                },
-                response => {
-                    utils.hideLoader();
-
-                    let message = "Sorry, something went wrong";
-                    if (response.status === 403) {
-                        message = "Please copy the sheet in your drive";
-                    }
-
-                    console.log(response);
-                    oSnackbar.MaterialSnackbar.showSnackbar({
-                        message,
-                        actionHandler: () => {
-                            window.open(
-                                "https://www.estateclaimservices.com/contact.html",
-                                "_blank"
-                            );
-                        },
-                        actionText: "Details",
-                        timeout: 5 * 60 * 1000
-                    });
-                }
-            );
-    }
+(function () {
 
 
     function init(sheetID, jobSplitTypes, jobClaimStatuses) {
@@ -111,6 +39,80 @@
         init,
     };
 })();
+
+
+function addJob(event) {
+    if (!AddPropertyForm.checkValidity()) return false;
+
+    alert("addJob 2: " + event); // !!!!!!!!!!!!!!
+
+    event.preventDefault();
+    utils.showLoader();
+
+    var now = new Date();
+
+    gapi.client.sheets.spreadsheets.values
+        .append(
+            utils.appendRequestObj([
+                [
+                    `=DATE(${now.getFullYear()}, ${now.getMonth()}, ${now.getDate()}) + TIME(${now.getHours()}, ${now.getMinutes()}, ${now.getSeconds()})`,
+                    oCustLastName.value,
+                    oCustFirstName.value,
+                    oCustAddr.value,
+                    oCustCity.value,
+                    oCustPhone.value,
+                    oCustEmail.value,
+                    oCustClaimNum.value,
+                    oJobSplitType.value,
+                    oJobScope.value,
+                    oJobNotes.value,
+                    oClaimStatus.value
+                ]
+            ])
+        )
+        .then(
+            response => {
+                // reset fileds
+                oCustLastName.value = "";
+                oCustFirstName.value = "";
+                oCustAddr.value = "";
+                oCustCity.value = "";
+                oCustPhone.value = "";
+                oCustEmail.value = "";
+                oCustClaimNum.value = "";
+                oJobSplitType.value = "";
+                oJobScope.value = "";
+                oJobNotes.value = "";
+                oClaimStatus.value = "";
+
+                oSnackbar.MaterialSnackbar.showSnackbar({
+                    message: "Property added!"
+                });
+                utils.hideLoader();
+            },
+            response => {
+                utils.hideLoader();
+
+                let message = "Sorry, something went wrong";
+                if (response.status === 403) {
+                    message = "Please copy the sheet in your drive";
+                }
+
+                console.log(response);
+                oSnackbar.MaterialSnackbar.showSnackbar({
+                    message,
+                    actionHandler: () => {
+                        window.open(
+                            "https://www.estateclaimservices.com/contact.html",
+                            "_blank"
+                        );
+                    },
+                    actionText: "Details",
+                    timeout: 5 * 60 * 1000
+                });
+            }
+        );
+}
 
 
 var oCamStrm;
