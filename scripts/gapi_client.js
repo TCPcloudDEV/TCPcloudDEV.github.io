a0da57ea4299c7bfad38d0d386518eff9b76f776
TCPcloudDEV <29411098+TCPcloudDEV@users.noreply.github.com>
2021-02-05 15:13:09 -0600

    DBG

--- scripts/gapi_client.js
+++ scripts/gapi_client.js
@@ -81,7 +81,7 @@ function authenticate() {
             processSignin();
         },
             function (err) {
-                alert("Failed to sign-in: " + err);
+                alert("Failed to sign-in2: " + err);
                 showError ("Failed to sign-in.", err);
             });
 }
